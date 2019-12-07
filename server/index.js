dd2c03b3cfdb04e98e58bc6c6ee107575b3995f0
Michael Lee <michaellee@Michaels-MBP-2.attlocal.net>
2019-12-07 15:56:53 -0800

    Added check for valid time

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const appointmentData = require('../appointmentData.json');
 const inMemoryDoctors = doctorData.slice();
 const inMemoryAppointments = appointmentData.slice();
 
-
+// Gets the next available appointment Id
 const getNextApptId = () => {
   return inMemoryAppointments[inMemoryAppointments.length - 1].id + 1;
 };
@@ -14,10 +14,12 @@ const getNextApptId = () => {
 app.use(bodyParser());
 app.use(express.static('dist'));
 
+//Sends all doctors
 app.get('/doctors', (req, res) => {
   res.send(inMemoryDoctors);
 })
 
+//Sends all appointments for a particular doctor on a particular date
 app.get('/appointments/:doctorId/:date', (req, res) => {
   let doctorId = req.params.doctorId;
   let date = req.params.date;
@@ -41,12 +43,15 @@ app.get('/appointments/:doctorId/:date', (req, res) => {
   res.send(appointments);
 });
 
+//Deletes an appointment for a particular doctor
 app.delete('/appointments/:doctorId/:apptId', (req, res) => {
   let doctorId = req.params.doctorId;
   let apptId = req.params.apptId;
   let doctorFirst;
   let doctorLast;
   let flag = false;
+
+  //Finds Doctor first and last name based on doctor id
   for (let i = 0; i < inMemoryDoctors.length; i++) {
     if (parseInt(doctorId) === inMemoryDoctors[i].id) {
       doctorFirst = inMemoryDoctors[i].firstName;
@@ -54,6 +59,9 @@ app.delete('/appointments/:doctorId/:apptId', (req, res) => {
       break;
     }
   }
+
+  //Finds appointment based on appointment id and doctor name
+  //This check is added in case someone includes an incorrect appointment id, so that it does not delete an appointment for another doctor on accident
   for (let i = 0; i < inMemoryAppointments.length; i++) {
     if (inMemoryAppointments[i].doctorFirst === doctorFirst && inMemoryAppointments[i].doctorLast === doctorLast) {
       if (parseInt(apptId) === inMemoryAppointments[i].id) {
@@ -70,8 +78,14 @@ app.delete('/appointments/:doctorId/:apptId', (req, res) => {
   }
 })
 
+//Creates a new appointment
 app.post('/appointments', (req, res) => {
+  let validTime = req.body.time.slice(3,5);
+  if (validTime !== '00' && validTime !== '15' && validTime !== '30' && validTime !== '45') {
+    return res.send('Appointments are only available every 15 minutes. Please reschedule your appointment to an appropriate time')
+  }
   let counterAtTime = 0;
+  //Finds how many appointments are already scheduled for this doctor at this time
   for (let i = 0; i < inMemoryAppointments.length; i++) {
     if (inMemoryAppointments[i].doctorId === parseInt(req.body.doctorId)) {
       if (inMemoryAppointments[i].date === req.body.date && inMemoryAppointments[i].time === req.body.time) {
@@ -79,6 +93,7 @@ app.post('/appointments', (req, res) => {
       }
     }
   }
+
   if (counterAtTime >= 3) {
     res.send('All appointment slots are booked for this time');
   } else {
